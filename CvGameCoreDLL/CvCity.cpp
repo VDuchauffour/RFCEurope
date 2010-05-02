3e97ce19974a4913be03ff3aec3870512fb6cf80
sedna17 <sedna17@56ccffae-a6bd-4c48-83f3-31c63dc0fc37>
2010-05-02 19:20:00 +0000

    Fixed Religious Prosecutor bug (introduced).

--- CvGameCoreDLL/CvCity.cpp
+++ CvGameCoreDLL/CvCity.cpp
@@ -817,7 +817,7 @@ void CvCity::kill(bool bUpdatePlotGroups)
 		abEspionageVisibility.push_back(getEspionageVisibility((TeamTypes)iI));
 	}
 
-	// JediClemente: unofficial patch
+// JediClemente: unofficial patch
 /************************************************************************************************/
 /* UNOFFICIAL_PATCH                       08/04/09                                jdog5000      */
 /*                                                                                              */
@@ -14150,31 +14150,6 @@ void CvCity::getBuildQueue(std::vector<std::string>& astrQueue) const
 	}
 }
 
-
-void CvCity::PurgeReligions(){
-	if ( (getX() == HOLIEST_CITY_X) && (getY() == HOLIEST_CITY_Y) ){
-		return;
-	};
-	int iStateReligion = GET_PLAYER(getOwner()).getStateReligion();
-	for( int i=0; i < NUM_RELIGIONS; i++ ){
-		if ( (i != iStateReligion) && (isHasReligion((ReligionTypes) i) ) && !isHolyCity( (ReligionTypes) i) ){
-			setHasReligion((ReligionTypes) i, false, false );
-			// if a building requires a religion that has been purged (i.e. non-state and non-holy city)
-			for( int j=0; j< GC.getNumBuildInfos(); j++ ){
-				if ( isHasRealBuilding((BuildingTypes) j) ){
-					if ( GC.getBuildingInfo((BuildingTypes)j).getPrereqReligion() == i ){
-							setHasRealBuilding( (BuildingTypes) j, false );
-					};
-				};
-			};
-		};
-	};
-
-	// 3MiroFaith
-	GET_PLAYER( getOwnerINLINE() ).changeFaith( 1 );
-	
-};
-
 void CvCity::changeDamageEnemy( int Change ){
 	m_iDamageEnemy += Change;
 };
@@ -14275,4 +14250,69 @@ int CvCity::getVotingPower( ReligionTypes eReligion ){
 
 	return ( getRealPopulation() * iReligionPower ) / ( 1000 + iReligionPower + iOtherPower );
 
+};
+
+bool CvCity::canPurgeReligion(){
+	if ( (getX() == HOLIEST_CITY_X) && (getY() == HOLIEST_CITY_Y) ){
+		return false;
+	};
+	int iStateReligion = GET_PLAYER(getOwner()).getStateReligion();
+	int iI, iJ;
+	bool safeReligion;
+	for( iI = 0; iI < NUM_RELIGIONS; iI++ ){
+		if ( (iI != iStateReligion) && (isHasReligion((ReligionTypes) iI) ) && !isHolyCity( (ReligionTypes) iI) ){
+			safeReligion = false;
+			for( iJ = 0; iJ < GC.getNumBuildingInfos(); iJ++ ){
+				if ( (hasBuilding((BuildingTypes) iJ)) && (GC.getBuildingInfo((BuildingTypes)iJ).getPrereqReligion() == iI) ){
+					if ( isLimitedWonderClass( (BuildingClassTypes) GC.getBuildingInfo((BuildingTypes)iJ).getBuildingClassType() ) ){
+						safeReligion = true;
+					};
+				};
+			};
+			if ( !safeReligion ){
+				return true;
+			};
+		};
+	};
+	return false;
+};
+
+
+void CvCity::doPurgeReligions(){
+	if ( !canPurgeReligion() ){
+		return;
+	};
+	/*if ( (getX() == HOLIEST_CITY_X) && (getY() == HOLIEST_CITY_Y) ){
+		return;
+	};*/
+	int iStateReligion = GET_PLAYER(getOwner()).getStateReligion();
+	bool safeReligion;
+	int iJ;
+	for( int i=0; i < NUM_RELIGIONS; i++ ){
+		if ( (i != iStateReligion) && (isHasReligion((ReligionTypes) i) ) && !isHolyCity( (ReligionTypes) i) ){
+			safeReligion = false;
+			for( iJ = 0; iJ < GC.getNumBuildingInfos(); iJ++ ){
+				if ( (hasBuilding((BuildingTypes) iJ)) && (GC.getBuildingInfo((BuildingTypes)iJ).getPrereqReligion() == i) ){
+					if ( isLimitedWonderClass( (BuildingClassTypes) GC.getBuildingInfo((BuildingTypes)iJ).getBuildingClassType() ) ){
+						safeReligion = true;
+					};
+				};
+			};
+			if ( !safeReligion ){
+				setHasReligion((ReligionTypes) i, false, false );
+				// if a building requires a religion that has been purged (i.e. non-state and non-holy city)
+				for( int j=0; j< GC.getNumBuildInfos(); j++ ){
+					if ( isHasRealBuilding((BuildingTypes) j) ){
+						if ( GC.getBuildingInfo((BuildingTypes)j).getPrereqReligion() == i ){
+								setHasRealBuilding( (BuildingTypes) j, false );
+						};
+					};
+				};
+			};
+		};
+	};
+
+	// 3MiroFaith
+	GET_PLAYER( getOwnerINLINE() ).changeFaith( 1 );
+	
 };
