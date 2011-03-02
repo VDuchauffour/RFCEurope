f6f6a3f65e2514f0001485663ab3955505fd3e2f
merijnv1 <merijnv1@56ccffae-a6bd-4c48-83f3-31c63dc0fc37>
2011-03-02 21:12:15 +0000

    Fixed Norse "colonial trade route bug" XML cleanup Python cleanup

--- RFCEurope/Assets/Python/Resources.py
+++ RFCEurope/Assets/Python/Resources.py
@@ -21,32 +21,19 @@ class Resources:
 			gc.getMap().plot(86, 2).setBonusType(xml.iRice)  #Rice in Middle East
 		if (iGameTurn == xml.i1500AD):
 			gc.getMap().plot(54, 35).setBonusType(xml.iRice) #Rice in Italy?
-                if ( iGameTurn == xml.i1050AD ):
-                        gc.getMap().plot( 2, 69).setBonusType(-1)  # Remove the AA from Iceland
+            if ( iGameTurn == xml.i1050AD ):
+                  gc.getMap().plot( 2, 69).setBonusType(-1)  # Remove the AA from Iceland
 		if (iGameTurn == xml.i1580AD): #1580
 			gc.getMap().plot(35, 58).setBonusType(xml.iPotato) #Potatoes in Ireland
 			gc.getMap().plot(37, 60).setBonusType(xml.iPotato)
 			gc.getMap().plot(68, 49).setBonusType(xml.iPotato) #Poland
 			gc.getMap().plot(59, 53).setBonusType(xml.iPotato) #Northern Germany
-                        gc.getMap().plot(49, 54).setBonusType(xml.iAccess) #Dutch AA
+                  gc.getMap().plot(49, 54).setBonusType(xml.iAccess) #Dutch AA
 		if (iGameTurn == xml.i1680AD):
 			gc.getMap().plot(59, 61).setBonusType(xml.iAccess) #Atlantic Access in Scandinavia
                         
 
 		# 3Miro: resources appear
-                #if (iGameTurn == 5): #otherwise it's picked by Portugal at the beginning
-                #        gc.getMap().plot(49, 43).setImprovementType(con.iHut)
-
-
-                #if (iGameTurn == con.i450AD): #(dye added later to prevent Carthaginian UHV exploit)
-                #        gc.getMap().plot(53, 51).setBonusType(iDye) #France
-                #        gc.getMap().plot(53, 55).setBonusType(iDye) #England
-                #if (not gc.getPlayer(0).isPlayable()): #late start condition
-                #        if (iGameTurn == con.i600AD): 
-                #                gc.getMap().plot(53, 51).setBonusType(iDye) #France
-                #                gc.getMap().plot(53, 55).setBonusType(iDye) #England
-                    
-                 
                 #setImprovementType(ImprovementType eNewValue)
                 #setPlotType(PlotType eNewValue, BOOL bRecalculate, BOOL bRebuildGraphics)
                 #setTerrainType(TerrainType eNewValue, BOOL bRecalculate, BOOL bRebuildGraphics)
