f27c6c50c300c66dde70050f3ed15abab0e989c4
threemiro <threemiro@56ccffae-a6bd-4c48-83f3-31c63dc0fc37>
2011-07-26 01:19:22 +0000

    More merc code, but nothing working yet.

--- RFCEurope/Assets/Python/Mercenaries.py
+++ RFCEurope/Assets/Python/Mercenaries.py
@@ -11,6 +11,8 @@ import XMLConsts as xml
 
 iNumPlayers = con.iNumPlayers
 
+iMercPromotion = 48
+
 #PyGame = PyHelpers.PyGame()
 
 # globals
@@ -22,6 +24,18 @@ iMercCostPerTurn = con.iMercCostPerTurn
 # list of all available mercs, unit type, text key name, start turn, end turn, provinces, blocked religions, odds
 lMercList = [ [xml.iArcher, "TXT_KEY_SERBIAN", 0, 100, [xml.iP_Serbia,xml.iP_IleDeFrance,xml.iP_Orleans], [], 10 ],
                 ]
+
+### A few Parameters for Mercs only:
+# Promotions and their odds, higher promotions have very low probability, leaders and navigation don't appear
+# combat 1 - 5, cover (vs archer), shock (vs heavy infantry), formation (vs heavy horse), charge (vs siege), ambush (vs light cav), feint (vs polearm), amphibious, march (movement heal), medic 1-2, 
+# gurilla (hill defense) 1-3, woodsman 1-3, city raider 1-3, garrason 1-3, drill 1-4, barrage (collateral) 1-3, accuracy (more bombard), flanking (vs siege) 1-2, sentry (vision), mobility (movement), 
+# navigation 1-2, leader, leadership (more XP), tactic (withdraw), commando (enemy roads), combat 6, morale (movement), medic 3, merc 
+lPromotionOdds = [ 100, 80, 40, 10,  5, 50, 50, 60, 40, 20, 50, 20, 10, 40, 20, 80, 50, 30, 80, 50, 30, 80, 40, 10, 60, 30, 10, 60, 40, 10,  5, 60, 40, 10, 60, 50, 30, 20, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+# The way promotions would affect the cost of the mercenary (percentage wise)
+lPromotionCost = [  10, 15, 30, 30, 40, 20, 20, 20, 20, 20, 20, 30, 40, 20, 30, 15, 20, 30, 15, 20, 30, 20, 30, 50, 20, 30, 50, 10, 20, 40, 50, 10, 10, 10, 20, 10, 10, 10, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+iNumPromotions = 39 # without navigation and leaders
+iNumPromotionsSoftCap = 3 # canget more promotions if you get a high promotion (i.e. combat 5), but overall it should be unlikely
+
 
 class MercenaryManager:
 
@@ -41,16 +55,51 @@ class MercenaryManager:
                 scriptDict['lMercsHiredBy'] = self.lHiredBy
                 gc.getGame().setScriptData( pickle.dumps(scriptDict) )
                 
+        def setPrereqConsistentPromotions( self, lPromotions ):
+                bPass = True
+                while ( not bPass ):
+                        bPass = True
+                        for iPromotion in lPromotions:
+                                pPromotionInfo = gc.getPromotionInfo( iPromotion )
+                                iPrereq = pPromotionInfo.getPrereqOrPromotion1()
+                                if ( iPrereq != -1 and ( not iPrereq in lPromotions ) ):
+                                        lPromotions.append( iPrereq )
+                                        bPass = False
+                                iPrereq = pPromotionInfo.getPrereqOrPromotion2()
+                                if ( iPrereq != -1 and ( not iPrereq in lPromotions ) ):
+                                        lPromotions.append( iPrereq )
+                                        bPass = False
+                return lPromotions
+                
         def addNewMerc( self, iMerc ):
                 # this processes the available promotions
+                lMercInfo = lMercList[iMerc]
+                
+                # get the promotions
+                iNumPromotions = 0
+                lPromotions = []
+                while ( iNumPromotions < iNumPromotionsSoftCap ):
+                        iPromotion = gc.getGame().getSorenRandNum( iNumPromotions, 'merc get promotion')
+                        if ( isPromotionValid(iPromotionInfoType, lMercInfo[0], False) ):
+                                if ( gc.getGame().getSorenRandNum( 100, 'merc set promotion') < lPromotionOdds[iPromotion] ):
+                                        lPromotions.append(iPromotion)
+                                        lPromotions = self.setPrereqConsistentPromotions( lPromotions )
+                                        iNumPromotions = len( lPromotions )
+                # compute cost?                        
+                                        
+                
                 pass
                 
         def processNewMercs( self, iGameTurn ):
                 # add new mercs to the pool
                 
                 potentialMercs = []
+                alreadyAvailableMercs = []
+                for iI in range( len( self.lGlobalPool ) ):
+                        alreadyAvailableMercs.append( self.lGlobalPool[iI][0] )
+                
                 for iMerc in range( len( lMercList ) ):
-                        if ( self.lHiredBy[iMerc] == -1 and iGameTurn >= lMercList[iMerc][2] and iGameTurn <= lMercList[iMerc][3] ):
+                        if ( self.lHiredBy[iMerc] == -1 and (not iMerc in alreadyAvailableMercs) and iGameTurn >= lMercList[iMerc][2] and iGameTurn <= lMercList[iMerc][3] ):
                                 potentialMercs.append( iMerc )
                 
                 iNumPotentialMercs = len( potentialMercs )
