7f96fe19891547fadec37e0252f57df49604c5ec
merijnv1 <merijnv1@56ccffae-a6bd-4c48-83f3-31c63dc0fc37>
2016-05-14 08:41:42 +0000

    UB for Portugal in 1200 AD scenario Hanseatic League HQ needs to be connected to a water area of at least 10 tiles.

--- Assets/Python/Barbs.py
+++ Assets/Python/Barbs.py
@@ -602,16 +602,16 @@ class Barbs:
 	def doIndependentCities(self, iGameTurn):
 		if iGameTurn in dIndependentCities.keys():
 			for tCity in dIndependentCities[iGameTurn]:
-				lPossibilities, iCiv, iPop, iUnit, iNumUnits, iReligion, iWorkers = tCity
+				lVariations, iCiv, iPop, iUnit, iNumUnits, iReligion, iWorkers = tCity
 				iChosenCity = 0
-				if len(lPossibilities) > 1:
+				if len(lVariations) > 1:
 					iRand = gc.getGame().getSorenRandNum(100, 'random independent city')
-					for iCity in range(len(lPossibilities)):
-						if iRand <= lPossibilities[iCity][2]:
+					for iCity in range(len(lVariations)):
+						if iRand <= lVariations[iCity][2]:
 							iChosenCity = iCity
 							break
-						iRand -= lPossibilities[iCity][2]
-				tCoords, sName, iPos = lPossibilities[iChosenCity]
+						iRand -= lVariations[iCity][2]
+				tCoords, sName, iPos = lVariations[iChosenCity]
 				self.foundCity(iCiv, tCoords, sName, iPop, iUnit, iNumUnits, iReligion, iWorkers)
 
 
