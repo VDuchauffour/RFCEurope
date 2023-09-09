f6228d3abf858d39731c1672598140aab1e1b179
VDuchauffour <vincent.duchauffour@proton.me>
2023-09-09 22:43:03 +0200

    Fix script

--- export-paths.sh
+++ export-paths.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-export PYTHONPATH=$(
-    tr -d $'\n ' <<<"
-    $SCRIPT_DIR/Assets/Python:
-    $SCRIPT_DIR/Assets/Python/models:
-    $SCRIPT_DIR/Assets/Python/EntryPoints:
-    $SCRIPT_DIR/Assets/Python/pyWB:
-    $SCRIPT_DIR/Assets/Python/screens:
-    $SCRIPT_DIR/../../Assets/Python:
-    $SCRIPT_DIR/../../Assets/Python/EntryPoints:
-    $SCRIPT_DIR/../../Assets/Python/pyWB:
-    $SCRIPT_DIR/../../Assets/Python/Screens:
-    $SCRIPT_DIR/../../Assets/Python/pyUnit:
-    $SCRIPT_DIR/../../Assets/Python/pyHelper:
-    $SCRIPT_DIR/../../Assets/Python/PitBoss:
-    $PYTHONPATH"
+PATHS=(
+	"$(readlink -f "$SCRIPT_DIR"/Assets/Python)"
+	"$(readlink -f "$SCRIPT_DIR"/Assets/Python/models)"
+	"$(readlink -f "$SCRIPT_DIR"/Assets/Python/EntryPoints)"
+	"$(readlink -f "$SCRIPT_DIR"/Assets/Python/pyWB)"
+	"$(readlink -f "$SCRIPT_DIR"/Assets/Python/screens)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/EntryPoints)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/pyWB)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/Screens)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/pyUnit)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/pyHelper)"
+	"$(readlink -f "$SCRIPT_DIR"/../../Assets/Python/PitBoss)"
+	"$PYTHONPATH"
 )
+
+for path in "${PATHS[@]}"; do
+	PYTHONPATH="$PYTHONPATH:$path"
+done
+
+echo "$PYTHONPATH"
