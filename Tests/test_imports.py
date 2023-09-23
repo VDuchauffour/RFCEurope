3701609ef4c0923375b88db64bb805ef5526519c
Vincent Duchauffour <vincent.duchauffour@proton.me>
2023-09-23 20:53:25 +0200

    Handle case with try..except for CvPythonExtensions in tests

--- Tests/test_imports.py
+++ Tests/test_imports.py
@@ -5,16 +5,30 @@ import unittest
 class TestDataMapper(unittest.TestCase):
     def setUp(self):
         cmd = subprocess.Popen(
-            ["/bin/bash", "-c", "grep -L 'CvPythonExtensions' Assets/Python/**/*.py"],
+            [
+                "/bin/bash",
+                "-c",
+                "grep -PzL '(from|import)\sCvPythonExtensions' Assets/Python/**/*.py",
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        self.outputs = cmd.communicate()[0]
+        cmd_2 = subprocess.Popen(
+            [
+                "/bin/bash",
+                "-c",
+                r"grep -Pzl 'try:\n\s+(from|import)\sCvPythonExtensions' Assets/Python/**/*.py",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        self.outputs = cmd.communicate()[0] + cmd_2.communicate()[0]
         self.outputs = list(
             map(lambda x: x.split("/")[-1].split(".py")[0], self.outputs.splitlines())  # type: ignore
         )
 
     def test_imports(self):
+        print(self.outputs)
         for output in self.outputs:
             __import__(output)  # type: ignore
 
