a08ac42327b7bce0b230fd928f8bc5ea63fbede3
Vincent Duchauffour <vincent.duchauffour@proton.me>
2023-11-19 23:34:05 +0100

    Fix typo

--- Assets/Python/data/TimelineData.py
+++ Assets/Python/data/TimelineData.py
@@ -312,7 +312,6 @@ CIV_RESPAWNING_DATE = CivDataMapper(
     }
 ).fill_missing_members(999)
 
-# TODO fix dates
 COMPANY_BIRTHDATE = CompanyDataMapper(
     {
         Company.HOSPITALLERS: DateTurn.i1096AD,
