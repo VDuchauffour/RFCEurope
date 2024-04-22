ddb3d2c19e779e22b647c1d7ddbf4eff581c2cf3
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-04-22 02:37:53 +0200

    Fix list of minor civs

--- Assets/Python/data/Consts.py
+++ Assets/Python/data/Consts.py
@@ -6,7 +6,7 @@ WORLD_HEIGHT = 73
 
 # Hardcoded because CyPlayer::isMinorCiv doesn't seems to work when the game starts
 INDEPENDENT_CIVS = [Civ.INDEPENDENT, Civ.INDEPENDENT_2, Civ.INDEPENDENT_3, Civ.INDEPENDENT_4]
-MINOR_CIVS = [INDEPENDENT_CIVS] + [Civ.BARBARIAN]
+MINOR_CIVS = INDEPENDENT_CIVS + [Civ.BARBARIAN]
 
 
 # Used for messages
