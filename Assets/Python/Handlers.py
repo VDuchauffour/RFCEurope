93b8b2b05a4611da6f65a216e27e09c683ca67c7
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-05-10 23:09:44 +0200

    Add handler in entrypoint

---
+++ Assets/Python/Handlers.py
@@ -0,0 +1,44 @@
+# ruff: noqa: F401
+
+import StoredData
+import Locations
+
+# import SettlerMaps
+# import WarMaps
+# import RegionMap
+import CityNameManager
+# import Civilizations
+import Modifiers
+# import AIParameters
+
+# import Scenarios
+# import ScreensHandler
+
+import AIWars
+import Barbs
+# import Civics
+# import Collapse
+# import Communications
+import Companies
+import Crusades
+import Mercenaries
+# import Congresses
+# import DynamicCivs
+# import EventSigns
+# import GreatPeople
+# import History
+# import Logging
+# import Messages
+# import Periods
+import Plague
+import Religions
+import Resources
+# import Resurrection
+# import Rise
+import RiseAndFall
+# import Rules
+# import Shortcuts
+import Stability
+import UniquePowers
+# import Victories
+# import Wonders
