0035b3e070152e767da2499d1a1bca87670d3cb8
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-05-12 19:18:59 +0200

    Add BUG for buffy, bug, contrib, pyhelper and cvutil.py

---
+++ Assets/Python/Contrib/PlotListEnhancements.py
@@ -0,0 +1,9 @@
+## PlotListEnhancements
+##
+## Utility functions for PLE by EmperorFool
+
+
+def resetUnitPlotListStackedBarColors(option, value):
+    import CvScreensInterface
+
+    CvScreensInterface.mainInterface.resetUnitPlotListStackedBarColors()
