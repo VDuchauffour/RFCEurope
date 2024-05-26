b51fed04a2f8118e2ce889935061d519a1793dfe
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-05-26 23:35:48 +0200

    Add platy settings

---
+++ Assets/Python/screens/PlatyBuilder/CvPlatyBuilderSettings.py
@@ -0,0 +1,14 @@
+iChange = 1
+bPython = True
+bHideInactive = True
+Activities = [
+    "AWAKE",
+    "HOLD",
+    "SLEEP",
+    "HEAL",
+    "SENTRY",
+    "INTERCEPT",
+    "MISSION",
+    "PATROL",
+    "PLUNDER",
+]
