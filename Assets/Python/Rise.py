eaabab99b3e1aff8593acd9383b77fa0472435bb
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2024-10-06 20:54:45 +0200

    Improve Dawn of Man screen (#67)
    * Improve dawn of Man screen
    
    * Add changelog
    
    * Bump version
    
    * Fix multiple dom

--- Assets/Python/Rise.py
+++ Assets/Python/Rise.py
@@ -1,8 +1,9 @@
 from Core import get_scenario_start_turn, game
 import CvScreensInterface
+from Events import handler
 
 
-# @handler("BeginGameTurn")
+@handler("BeginGameTurn")
 def showDawnOfMan(iGameTurn):
     if iGameTurn == get_scenario_start_turn() and game.getAIAutoPlay() > 0:
         CvScreensInterface.dawnOfMan.interfaceScreen()
